2242506e49e8bbc05458ff48de9121a9ac0a71ef
Harsha Nanjundaiaha <harsha.tumkur@gmail.com>
2016-05-16 20:04:14 +0530

    updated

---
+++ mywork/Trees/Diameter.java
@@ -0,0 +1,26 @@
+/*
+The diameter of a tree T is the largest of the following quantities:
+
+* the diameter of T’s left subtree
+* the diameter of T’s right subtree
+* the longest path between leaves that goes through the root of T (this can be
+*   computed from the heights of the subtrees of T)
+*/
+
+public int getDiameter(Node root) {
+    if(root == null) return 0;
+
+    int lh = getHeight(root.left);
+    int rh = getHeight(root.right);
+
+    int ld = getDiameter(root.left);
+    int rd = getDiameter(root.left);
+
+    /*
+    Return max of following three
+              1) Diameter of left subtree
+             2) Diameter of right subtree
+             3) Height of left subtree + height of right subtree + 1    
+    */
+    return Math.max(lh+rh+1, Math.max(ld, rd));
+}
---
+++ mywork/Trees/RootToLeafPathToSum.java
@@ -0,0 +1,35 @@
+/*
+    RootToLeafPathToSum
+
+    Definition: The sum of the data from 'root' to 'leaf' equals to SUM
+
+    For example, in the above tree root to leaf paths exist with following sums.
+
+    21 –> 10 – 8 – 3
+    23 –> 10 – 8 – 5
+    14 –> 10 – 2 – 2
+
+    So the returned value should be true only for numbers 21, 23 and 14. For any
+     other number, returned value should be false.
+
+    Algorithm:
+    Recursively check if left or right child has path sum equal to ( number –
+        value at current node)
+
+*/
+
+public boolean hasRootToLeafPathToSum(Node root, int sum) {
+    if(root == null) {
+        return (sum == 0);
+    } else {
+        boolean result = false;
+
+        int subsum = sum - root.data;
+        if(subsum == 0 && root.left == null && root.right == null) {
+            return true;
+        }
+
+        return (hasRootToLeafPathToSum(root.left, subsum) ||
+                hasRootToLeafPathToSum(root.right, subsum));
+    }
+}

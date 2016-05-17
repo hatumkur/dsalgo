be5b357c29143367b2d1f38b921a2ead29cc8b7d
Harsha Nanjundaiaha <harsha.tumkur@gmail.com>
2016-05-17 14:20:19 +0530

    CountPathWithSum solution added

---
+++ mywork/Trees/PathsWithSum.java
@@ -0,0 +1,30 @@
+/*
+   Return number of paths in a Binary tree which add up to a given SUM
+*/
+
+public int countPathWithSum(Node root, int sum) {
+    if(root == null) return 0;
+
+    int totalRootCount = countPathWithSum(root, sum, 0);
+
+    int totalLeftCount = countPathWithSum(root, sum);
+    int totalRightCount = countPathWithSum(root, sum);
+    return 0;
+}
+
+private int countPathWithSum(Node root, int sum, int currSum) {
+    if(root == null) return 0;
+
+    int totalCount = 0;
+    currSum += root.data;
+    if(currSum == sum){
+        totalCount++;
+    } else if(currSum > sum) {
+        return totalCount;
+    }
+
+    totalCount += countPathWithSum(root.left, sum, currSum);
+    totalCount += countPathWithSum(root.right, sum, currSum);
+
+    return totalCount;
+}
